12a9ccaebecf331a11e08e291d0e1826e2a8004b
Grey <grey@grayespinoza.dev>
2026-05-03 22:59:12 -0700

    feat(gitea): add setup script

---
+++ gitea/setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -eu
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y ufw
+
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+sudo ufw enable
+
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo usermod -aG docker $USER
+
+sudo ufw allow 3000/tcp
+sudo ufw allow 2222/tcp
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+DIRS=(
+  "data"
+  "config"
+)
+
+for dir in "${DIRS[@]}"; do
+  mkdir -p "$SCRIPT_DIR/$dir"
+  sudo chown -R 1000:1000 "$SCRIPT_DIR/$dir"
+done
