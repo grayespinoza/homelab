c0ff6d51e47210b293c386635fa7143491fc69d1
Grey <grey@grayespinoza.dev>
2026-05-25 13:49:51 -0700

    feat(fireshare): add setup script

---
+++ fireshare/setup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -eu
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y ufw
+
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+sudo ufw enable
+
+sudo apt install -y unattended-upgrades apt-listchanges
+
+sudo tee /etc/apt/apt.conf.d/20auto-upgrades <<EOF
+APT::Periodic::AutocleanInterval "7";
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "1";
+EOF
+
+sudo tee /etc/apt/apt.conf.d/50unattended-upgrades <<EOF
+Unattended-Upgrade::Origins-Pattern {
+  "origin=Debian,codename=${distro_codename},label=Debian";
+  "origin=Debian,codename=${distro_codename}-security,label=Debian-Security";
+  "origin=Debian,codename=${distro_codename},label=Debian-Security";
+};
+Unattended-Upgrade::Remove-Unused-Dependencies "true";
+Unattended-Upgrade::Automatic-Reboot "false";
+EOF
+
+sudo systemctl enable unattended-upgrades
+
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo usermod -aG docker $USER
+
+sudo ufw allow 8080/tcp
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+mkdir -p "$SCRIPT_DIR/config"
+sudo chown -R 1000:1000 "$SCRIPT_DIR/config"
+
+DIRS=(
+  "mnt/media/games/videos"
+  "mnt/media/games/images"
+)
+
+for dir in "${DIRS[@]}"; do
+  sudo mkdir -p "/$dir"
+done
+sudo chown -R 1000:1000 "/mnt/media"
+
+sudo apt install -y nfs-common
